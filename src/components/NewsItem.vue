2ecbce3551f9ddabdb87595f6155557b53c9d719
Serhii Dmytrakov <clamdm@gmail.com>
2022-09-30 10:02:24 +0300

    Add local list news on Home page

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="row">
+    <div class="col s12">
+      <div class="card card-responsive">
+        <div class="card-content">
+          <span class="card-title grey-text text-darken-4">{{snippet.title}}</span>
+          <span class="card-subtitle grey-text text-darken-4"
+            >{{snippet.category}}</span
+          >
+          <div class="card-content">
+            <p>{{snippet.text}}</p>
+          </div>
+          <div>
+            <button
+              class="waves-effect waves-light btn-small"
+              type="submit"
+              name="action"
+            >
+              <i class="large material-icons">edit</i>
+            </button>
+            <button
+              class="waves-effect waves-light btn-small"
+              type="submit"
+              name="action"
+            >
+              <i class="large material-icons">more</i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+</template>
+  
+  <script>
+// @ is an alias to /src
+
+export default {
+  props: {
+    snippet: {
+      type: Object,
+      required: true
+    }
+  },
+  name: "NewsList",
+  components: {},
+};
+</script>
+  
+  <style>
+.card-responsive {
+  position: relative;
+}
+
+button {
+  margin: 5px;
+  opacity: 0;
+  position: absolute;
+  transition: opacity 0.3s;
+}
+
+.card-responsive:hover button {
+  opacity: 1;
+  transition: opacity 0.3s;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>
-    Text
+  <div class="container">
+    <NewsList
+      v-bind:snippets="snippets"
+    />    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import NewsList from '@/views/NewsList.vue'
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      snippets: [
+        {id: 1, title: 'Новость 1', category: 'Финансы', text: 'I am a very simple car'},
+        {id: 2, title: 'Новость 2', category: 'Спорт', text: 'I am good at containing small bits of information'},
+        {id: 3, title: 'Новость 3', category: 'Культура', text: 'I am convenient because I require little markup to use effectively.'}
+      ]
+    }
+  },
   components: {
-    
-  }
-}
+    NewsList
+  },
+};
 </script>
+
+<style>
+.card-responsive {
+  position: relative;
+}
+
+button {
+  opacity: 0;
+  position: absolute;
+  transition: opacity 0.3s;
+}
+
+.card-responsive:hover button {
+  opacity: 1;
+  transition: opacity 0.3s;
+}
+</style>
